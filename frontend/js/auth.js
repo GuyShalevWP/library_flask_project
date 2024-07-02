228941f6039c9392145e938711ce59fa488959d5
Guy <106748288+GuySha123@users.noreply.github.com>
2024-07-02 15:27:04 +0300

    sginin frontend updated

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -1,110 +1,91 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const SERVER = 'http://localhost:7000';
-    const signinForm = document.getElementById('signinForm');
-    const registerForm = document.getElementById('registerForm');
+const SERVER = 'http://localhost:7000';
+const registerForm = document.getElementById('registerForm');
 
-    if (signinForm) {
-        signinForm.addEventListener('submit', async (event) => {
-            event.preventDefault();
-            const email = document.getElementById('email').value;
-            const password = document.getElementById('password').value;
+const login = () => {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const message = document.getElementById('message');
 
-            console.log('Sign In Attempt:', { email, password });
+    axios
+        .post(`${SERVER}/login`, {
+            email: email,
+            password: password,
+        })
+        .then((response) => {
+            const token = response.data.access_token;
+            const msg = response.data.message;
 
-            try {
-                const response = await axios.post(`${SERVER}/login`, {
-                    email,
-                    password,
-                });
+            localStorage.setItem('token', token); // Store token in localStorage
 
-                if (response.status === 200) {
-                    document.getElementById('message').innerHTML =
-                        '<div class="alert alert-success">Login successful</div>';
-                    // Store the token in localStorage
-                    localStorage.setItem('token', response.data.access_token);
+            message.innerHTML = `<div class="alert alert-success">${msg}</div>`;
 
-                    // Store user details in session storage
-                    sessionStorage.setItem(
-                        'user',
-                        JSON.stringify(response.data.user)
-                    );
+            // Wait for 1 second before redirecting
+            setTimeout(() => {
+                window.location.href = '../../index.html'; // Redirect to home page
+            }, 1000);
+        })
 
-                    // Redirect to home page
-                    window.location.href = '../../index.html';
-                } else {
-                    document.getElementById('message').innerHTML =
-                        '<div class="alert alert-danger">Invalid email or password</div>';
-                }
-            } catch (error) {
-                console.error('Error during login:', error); // Log error to console
-                const errorMessage =
-                    error.response &&
-                    error.response.data &&
-                    error.response.data.message
-                        ? error.response.data.message
-                        : 'Invalid email or password';
-                document.getElementById(
-                    'message'
-                ).innerHTML = `<div class="alert alert-danger">${errorMessage}</div>`;
-            }
+        .catch((error) => {
+            console.error('Error during login:', error);
+            const errorMessage =
+                error.response?.data?.message || 'Invalid email or password';
+            message.innerHTML = `<div class="alert alert-danger">${errorMessage}</div>`;
         });
+};
+
+const register = async (event) => {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const retype_password = document.getElementById('retype_password').value;
+    const first_name = document.getElementById('first_name').value;
+    const last_name = document.getElementById('last_name').value;
+    const phone = document.getElementById('phone').value;
+
+    if (password !== retype_password) {
+        document.getElementById('message').innerHTML =
+            '<div class="alert alert-danger">Passwords do not match</div>';
+        return;
     }
 
-    if (registerForm) {
-        registerForm.addEventListener('submit', async (event) => {
-            event.preventDefault();
-            const email = document.getElementById('email').value;
-            const password = document.getElementById('password').value;
-            const retype_password =
-                document.getElementById('retype_password').value;
-            const first_name = document.getElementById('first_name').value;
-            const last_name = document.getElementById('last_name').value;
-            const phone = document.getElementById('phone').value;
+    console.log('Registration Attempt:', {
+        email,
+        password,
+        first_name,
+        last_name,
+        phone,
+    });
 
-            if (password !== retype_password) {
-                document.getElementById('message').innerHTML =
-                    '<div class="alert alert-danger">Passwords do not match</div>';
-                return;
-            }
-
-            console.log('Registration Attempt:', {
-                email,
-                password,
-                first_name,
-                last_name,
-                phone,
-            });
-
-            try {
-                const response = await axios.post(`${SERVER}/register`, {
-                    email,
-                    password,
-                    first_name,
-                    last_name,
-                    phone,
-                });
-
-                if (response.status === 201) {
-                    document.getElementById('message').innerHTML =
-                        '<div class="alert alert-success">Registration successful</div>';
-                    // Redirect to sign-in page
-                    window.location.href = 'signin.html';
-                } else {
-                    document.getElementById('message').innerHTML =
-                        '<div class="alert alert-danger">Registration failed</div>';
-                }
-            } catch (error) {
-                console.error('Error during registration:', error);
-                const errorMessage =
-                    error.response &&
-                    error.response.data &&
-                    error.response.data.message
-                        ? error.response.data.message
-                        : 'Registration failed';
-                document.getElementById(
-                    'message'
-                ).innerHTML = `<div class="alert alert-danger">${errorMessage}</div>`;
-            }
+    try {
+        const response = await axios.post(`${SERVER}/register`, {
+            email,
+            password,
+            first_name,
+            last_name,
+            phone,
         });
+
+        if (response.status === 201) {
+            document.getElementById('message').innerHTML =
+                '<div class="alert alert-success">Registration successful</div>';
+            // Redirect to sign-in page
+            window.location.href = 'signin.html';
+        } else {
+            document.getElementById('message').innerHTML =
+                '<div class="alert alert-danger">Registration failed</div>';
+        }
+    } catch (error) {
+        console.error('Error during registration:', error);
+        const errorMessage =
+            error.response && error.response.data && error.response.data.message
+                ? error.response.data.message
+                : 'Registration failed';
+        document.getElementById(
+            'message'
+        ).innerHTML = `<div class="alert alert-danger">${errorMessage}</div>`;
     }
-});
+};
+
+if (registerForm) {
+    registerForm.addEventListener('submit', register);
+}
